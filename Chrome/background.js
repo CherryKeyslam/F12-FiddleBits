3fc7b259be312ed6a276f910b8b5ebe0a12b6b54
CherryKeyslam <85491783+CherryKeyslam@users.noreply.github.com>
2021-07-08 09:14:52 +0530

    Add files via upload

---
+++ Chrome/background.js
@@ -0,0 +1,44 @@
+chrome.contextMenus.create({
+  title: "Highlight", 
+  contexts:["selection"], 
+  id: "highlight"
+});
+chrome.contextMenus.create({
+  title: "Reddify", 
+  contexts:["selection"], 
+  id: "reddify"
+});
+chrome.contextMenus.create({
+  title: "Blunt", 
+  contexts:["selection"], 
+  id: "bolden"
+});
+chrome.contextMenus.create({
+  title: "Imprint Selection", 
+  contexts:["selection"], 
+  id: "imprint"
+});
+
+chrome.contextMenus.onClicked.addListener(
+(info,tab) => {
+  if(info.menuItemId == "highlight") {
+    chrome.tabs.sendMessage(tab.id, {command: "highlight"},function(response) {
+      console.log('Operation success. Command sent and received: Highlight ');
+  })
+  }
+  else if(info.menuItemId == "reddify") {
+    chrome.tabs.sendMessage(tab.id, {command: "reddify"},function(response) {
+      console.log('Operation success. Command sent and received: Reddify ');
+    });
+  }
+  else if(info.menuItemId == "bolden") {
+    chrome.tabs.sendMessage(tab.id, {command: "bolden"},function(response) {
+      console.log('Operation success. Command sent and received: Blunt ');
+    });
+  }
+  else if(info.menuItemId == "imprint") {
+    chrome.tabs.sendMessage(tab.id, {command: "imprint"},function(response) {
+      console.log('Operation success. Command sent and received: Imprint selection ');
+    });
+  }
+});
---
+++ Chrome/content.js
@@ -0,0 +1,24 @@
+chrome.runtime.onMessage.addListener(
+ function(request, sender, sendResponse) {
+    let selection = window.getSelection();
+    let selectionText = selection.toString();
+    let range = selection.getRangeAt(0);
+    range.deleteContents();
+    let newNode = document.createElement("span");
+    if(request.command == "highlight") {
+       newNode.style.backgroundColor = "yellow";
+    }
+    else if(request.command == "reddify") {
+       newNode.style.color = "red";
+    }
+    else if(request.command == "bolden") {
+       newNode.style.fontWeight = "bold";
+    }
+    else if(request.command == "imprint") {
+       newNode.style.color = "white";
+       newNode.style.backgroundColor = "#338FFF";
+    }
+    newNode.appendChild(document.createTextNode(selectionText));
+    range.insertNode(newNode);
+    return true;
+});
